7943ef325913e8db4f5ddb7aa6631658de87903d
Andre Susanto <andre@susan.to>
2021-09-04 14:54:23 +0800

    Implement health check

--- ingester/cmd/server/main.go
+++ ingester/cmd/server/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/andresusanto/aws-elastic-search-demo/ingester/internal/config"
 	"github.com/andresusanto/aws-elastic-search-demo/ingester/internal/es"
 	"github.com/andresusanto/aws-elastic-search-demo/ingester/internal/logging"
 	"github.com/andresusanto/aws-elastic-search-demo/ingester/pkg/events"
+	"github.com/andresusanto/aws-elastic-search-demo/ingester/pkg/healthcheck"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
@@ -25,7 +29,18 @@ func main() {
 		log.Fatal().Err(err).Msg("cannot connect to elastic search")
 	}
 
+	h := healthcheck.RegisterHandler(r)
 	events.RegisterHandler(r, events.NewService(events.NewRepository(es)))
 
-	r.Run(fmt.Sprintf(":%d", cfg.Port))
+	go func() {
+		err := r.Run(fmt.Sprintf(":%d", cfg.Port))
+		log.Fatal().Err(err).Msg("cannot start HTTP server")
+	}()
+
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c
+	log.Info().Msg("SIGTERM received. Performing graceful shutdown.")
+	h.Shutdown()
 }
--- ingester/pkg/healthcheck/api.go
+++ ingester/pkg/healthcheck/api.go
@@ -1,1 +1,38 @@
 package healthcheck
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Handler performs graceful shutdown and processes health check requests
+type Handler interface {
+	Shutdown()
+}
+
+// RegisterHandler sets up the routing of the HTTP handlers.
+func RegisterHandler(r *gin.Engine) Handler {
+	h := &handler{true}
+	r.GET("/health", h.get)
+	return h
+}
+
+type handler struct {
+	ok bool
+}
+
+func (h *handler) Shutdown() {
+	h.ok = false
+	time.Sleep(10 * time.Second)
+}
+
+func (h *handler) get(c *gin.Context) {
+	if h.ok {
+		c.Status(http.StatusOK)
+		return
+	}
+
+	c.Status(http.StatusServiceUnavailable)
+}
